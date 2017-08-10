339012fcaa50471c7e06cab8f4b5a96aafcf99e7
Jiayang <Jiayang@apples-MacBook-Pro.local>
2017-08-10 09:59:28 -0400

    Adds port number

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.get('/', function (req, res) {
 	res.sendfile(html);
 })
 
+app.use('/scripts', express.static(__dirname + '/scripts'));
+
 app.listen(3000, function() {
-	console.log("Testing express app..");
+	console.log("Testing express app.. on localhost:3000s");
 })
